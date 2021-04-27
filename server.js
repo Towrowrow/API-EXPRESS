05aeeb063128c9b3f8755144f2b35b5e63da0f1b
Rémi <remi.tilmant@metsys.fr>
2021-04-27 11:15:48 +0200

    [RTI] Init Express server file and Add type module for import

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('Hello World')
+import express from "express"
+
+const PORT = 3000
+
+const app = express()
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
