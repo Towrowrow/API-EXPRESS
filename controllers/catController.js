4c0b679fa7e81c1055ffabee0d70ece1a0ec2e49
Rémi <remi.tilmant@metsys.fr>
2021-04-28 10:26:47 +0200

    [RTI] CRUD for  Cat Controller

--- controllers/catController.js
+++ controllers/catController.js
@@ -1,16 +1,44 @@
 import CatModel from '../models/catModel.js'
 
-export const getCat = (_, res) => {
-    res.send('Get Cat')
-}
 
-export const postCat  = (req, res) => {
-    res.send('Post Cast')
-}
 
+//ADD 
 export const addCat = async (req, res) => {
     const cat = new CatModel(req.body)
     await cat.save()
     res.send(cat)
+}
 
+//Read All
+export const getCats = async (req, res) => {
+    const cats = await CatModel.find({})
+    res.send(cats)
+}
+
+//Read One
+export const getCatById = async (req, res) => {
+    const cat = await CatModel.find({_id : req.params.id})
+    res.send(cat)
+}
+
+//Update
+export const updateCat = async (req, res) => {
+    //Pass {new :true} option on FBIAU for get update one entity, default:False.
+    const cat = await CatModel
+        .findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true },
+            (e)=>res.send(e.message)
+        )
+    await cat.save()
+    res.send(cat)
+}
+//DELETE
+export const removeCat = async (req, res) => {
+    const cat = await CatModel.findByIdAndDelete(req.params.id)
+    if (!cat) {
+        res.status(404).send('Item not found')
+    }
+    res.status(200)
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,20 @@
 import express from 'express'
-import { addCat, getCat, postCat } from '../controllers/catController.js'
+import { addCat, getCatById, getCats, removeCat, updateCat } from '../controllers/catController.js'
 import { catchErrors } from '../helpers.js'
+
 const router = express.Router()
 
 router.get('/', (_, res) => {
     res.send('Hello World')
 })
-router.get('/test', getCat)
-router.post('/test', postCat)
-router.post('/cat',catchErrors(addCat) )
+//GET
+router.get('/cat/:id', catchErrors(getCatById))
+router.get('/cats', catchErrors(getCats))
+//POST
+router.post('/cat', catchErrors(addCat))
+//PUT/PATCH
+router.patch('/cat/:id',catchErrors(updateCat))
+//DELETE
+router.delete('/cat/:id',catchErrors(removeCat))
 
 export default router
