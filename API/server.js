79132405ad3d4e8510d69aff51eb75f2aed3db55
Rémi <remi.tilmant@metsys.fr>
2021-05-04 14:27:30 +0200

    [RTI] Add cors dependency for local use

--- API/server.js
+++ API/server.js
@@ -1,28 +1,25 @@
-import dotenv from "dotenv"
-import express from "express"
-import mongoose from 'mongoose'
-import { catController } from './Controllers/catController.js'
-import { dogController } from './Controllers/dogController.js'
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import { catController } from "./Controllers/catController.js";
+import { dogController } from "./Controllers/dogController.js";
+dotenv.config();
 
-dotenv.config()
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const PORT = process.env.PORT || 3000
-
-const app = express()
-app.use(express.json())
-
-
-app.use('/cat',catController)
-app.use('/dog',dogController)
-
+app.use("/cat", catController);
+app.use("/dog", dogController);
 
 mongoose.connect(process.env.MONGODB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify:false
-})
-
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
