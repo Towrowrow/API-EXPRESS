c0dbd414cc47764df375e90b518b0c0454461f58
Rémi <remi.tilmant@metsys.fr>
2021-05-04 14:44:02 +0200

    [RTI] Update requireD on model field

--- Data/models/catModel.js
+++ Data/models/catModel.js
@@ -1,23 +1,23 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const CatSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        require: true,
-        trim: true,
-        lowerCase:true
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    lowerCase: true,
+  },
+  age: {
+    type: Number,
+    default: 1,
+    validate: (value) => {
+      if (value <= 0) {
+        throw new Error("Cat must be older than 0");
+      }
     },
-    age: {
-        type: Number,
-        default: 1,
-        validate: value => {
-            if (value <= 0) {
-                throw new Error('Cat must be older than 0')
-            }
-        }
-    }
-})
+  },
+});
 
-const Cat = mongoose.model('Cat', CatSchema)
+const Cat = mongoose.model("Cat", CatSchema);
 
-export default Cat
+export default Cat;
